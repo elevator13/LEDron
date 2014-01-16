7155e95ca6b80da9a8b8723e5d692e5acf2f682d
Tommy Klein <twklein@andrew.cmu.edu>
2014-01-16 01:32:38 -0500

    First changes...

--- cube_testing.ino
+++ cube_testing.ino
@@ -1,18 +1,13 @@
-int ledPin = 13;
-//int baseChip = 8; //9,10,
+int ledPin = 13; //Don't use this
 
 int baseChip = 0;
 int counter = 0;
 
-int latchPin = 11; //OE
-//int clockPin = 8;
-//int dataPin = 9;
+int latchPin = 11;
 
 int clock1Pin = 8;
 int clock2Pin = 9;
 int clock3Pin = 10;
-//int baseChip = 0;
-//int counter = 0;
 //first 8 (pins 0-7, from basechip): y
 //clocks (pins 8-10): x
 //otherPins (12,13...,A5): z
@@ -21,7 +16,9 @@ int otherPins[] = {12,13,A0,A1,A2,A3,A4,A5};
 //byte[8][8][8] isOn;
 int curLayer;
 
-
+/*Turn voltage to a pin on/off,
+ *depending on the argument "output"
+ */
 void bto(int pin, int output)
 {
   if (output != 0)
@@ -44,7 +41,6 @@ void setup()
   curLayer = 0;
   int i = 0;
   pinMode(latchPin, OUTPUT);
-  //pinMode(ledPin,OUTPUT);
   pinMode(clock1Pin,OUTPUT);
   pinMode(clock2Pin,OUTPUT);
   pinMode(clock3Pin,OUTPUT);
@@ -59,18 +55,12 @@ void setup()
   digitalWrite(clock1Pin,LOW);
   digitalWrite(clock2Pin,LOW);
   digitalWrite(clock3Pin,LOW);
-  
-  //digitalWrite(12,LOW);
-  //digitalWrite(13,LOW);
-  
   for (i = 0; i < 8; i++)
   {
     pinMode(otherPins[i], OUTPUT);
     digitalWrite(otherPins[i], LOW);
   }
   //digitalWrite(latchPin,LOW);
-  //digitalWrite(1,HIGH);
-  //digitalWrite(13,HIGH);
   digitalWrite(curLayer,HIGH);
 }
 
@@ -78,7 +68,10 @@ void loop()
 {
   if (curLayer == 8)
     return;
-  delay(200);
+  delay(300);
+  /*Cycle the clocks.  Will iterate from 0 to 7, then
+   *switch to a new layer and repeat.
+   */
   bto(clock1Pin, counter & 1);
   bto(clock2Pin, counter & 2);
   bto(clock3Pin, counter & 4);
@@ -86,6 +79,8 @@ void loop()
   if (counter >= 8)
   {
     counter = 0;
+    /*Will only turn on one column at a time,
+     *instead of continuously lighting new columns.*/
     //digitalWrite(curLayer,LOW);
     curLayer++;
     curLayer = curLayer % 8;
